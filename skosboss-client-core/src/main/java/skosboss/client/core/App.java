69b890f085ddd04c2eaea8aade62e55ff130c769
Victor Louwerse <victor.louwerse@taxonic.com>
2017-07-27 17:00:21 +0200

    Do example shacl validation

---
+++ skosboss-client-core/src/main/java/skosboss/client/core/App.java
@@ -0,0 +1,32 @@
+package skosboss.client.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.topbraid.shacl.validation.ValidationUtil;
+
+public class App {
+
+	public static void main(String... args) {
+		
+		Model data = loadModel("test.ttl");
+		Model shapes = loadModel("test.sh.ttl");
+		
+		Resource result = ValidationUtil.validateModel(data, shapes, false);
+		result.getModel().write(System.out, "TURTLE");
+		
+	}
+	
+	private static Model loadModel(String name) {
+		try (InputStream input = App.class.getClassLoader().getResourceAsStream(name)) {
+			return ModelFactory.createDefaultModel()
+				.read(input, "http://none.com/", "TURTLE");
+		}
+		catch (IOException e) {
+			throw new RuntimeException("failed to load model from resource [" + name + "]", e);
+		}
+	}
+}
